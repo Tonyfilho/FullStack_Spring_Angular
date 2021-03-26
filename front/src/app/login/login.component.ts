b05c71cac857624f626eed130a6ed9202334f6d4
tony filho <antonio.m.filho>
2021-03-26 08:41:32 +0000

    1º stage

---
+++ front/src/app/login/login.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Country {
+  id: number;
+  name: string;
+  flag: string;
+  area: number;
+  population: number;
+}
+const COUNTRIES: Country[] = [
+  {
+    id: 1,
+    name: 'Russia',
+    flag: 'f/f3/Flag_of_Russia.svg',
+    area: 17075200,
+    population: 146989754
+  },
+  {
+    id: 2,
+    name: 'Canada',
+    flag: 'c/cf/Flag_of_Canada.svg',
+    area: 9976140,
+    population: 36624199
+  },
+  {
+    id: 3,
+    name: 'United States',
+    flag: 'a/a4/Flag_of_the_United_States.svg',
+    area: 9629091,
+    population: 324459463
+  },
+  {
+    id: 4,
+    name: 'China',
+    flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
+    area: 9596960,
+    population: 1409517397
+  }
+];
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  countries: Country[] = COUNTRIES;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
